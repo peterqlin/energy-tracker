b5d0b3488ca7c2fc84db887b40429f77fa42313d
230pelin <peter.lin2305@gmail.com>
2024-11-29 12:35:52 -0600

    Implemented mouse tracking with variable screen size. Added offset from line to cursor. Removed some redundant files

--- src/Components/InteractiveCircle.js
+++ src/Components/InteractiveCircle.js
@@ -1,39 +1,66 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 const InteractiveCircle = () => {
-  // line logic
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const circleRef = useRef();
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [circlePosition, setCirclePosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    const boundingRect = circleRef.current.getBoundingClientRect();
-    const circleX = (boundingRect['left'] + boundingRect['right']) / 2;
-    const circleY = (boundingRect['top'] + boundingRect['bottom']) / 2;
+    // set the mousePosition variable whenever the cursor is moved, accounting for the offset of the circle
+    // the line should point to the cursor
     const handleMoveMouse = (event) => {
-      setMousePosition({ x: event.clientX - circleX, y: event.clientY - circleY });
+      setMousePosition(() => {
+        const angle = Math.atan(event.clientY, event.clientX);
+        console.log(angle);
+        let offset = 10;
+        if (event.clientY - circlePosition.y < 0) {
+          offset *= -1;
+        }
+        return { x: event.clientX - circlePosition.x - offset * Math.cos(angle), y: event.clientY - circlePosition.y - offset * Math.sin(angle) };
+      });
     };
+
     window.addEventListener('mousemove', handleMoveMouse);
+
     return () => {
-      window.addEventListener('mousemove', handleMoveMouse);
+      window.removeEventListener('mousemove', handleMoveMouse);
+    };
+  }, [circlePosition]);
+
+  useEffect(() => {
+    // make it so resizing the window does not offset where the line points to
+    const handleResize = () => {
+      const boundingRect = circleRef.current.getBoundingClientRect();
+      const circleX = (boundingRect['left'] + boundingRect['right']) / 2;
+      const circleY = (boundingRect['top'] + boundingRect['bottom']) / 2;
+      setCirclePosition({ x: circleX, y: circleY });
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   return (
-    <>
-      <div ref={circleRef} className='circle'></div>
-      <div className='line-container'>
+    <div className='interactive-circle-container'>
+      <div ref={circleRef} className='centered-component circle'></div>
+      <div className='centered-component line-container'>
         <svg>
-          <line className='line'
+          <line
             x1={0}
             y1={0}
             x2={mousePosition.x}
             y2={mousePosition.y}
             stroke='black'
-            strokeWidth='5'
+            strokeWidth='20'
           />
         </svg>
       </div>
-    </>
+    </div>
   );
 };
 
